8094e7fd5230244dca63f06a518f433b36a7dcb9
Steve Koutsodontis <skoutsodontis@gmail.com>
2021-11-05 19:02:03 -0400

    Added descriptions to the javascript.index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,25 +1,25 @@
-
+// Attaching the Javascript to the HTML.
 var generatePasswordBtn = document.getElementById("generate");
-
+// Special Character , Letters and Number Strings.
 var specialCharStr = "~!@#$%^&*()_+-="; 
 var uppercaseLetterStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowercaseLetterStr = "abcdefghijklmnopqrstuvwxyz";
 var numberStr = "0123456789";
-
+//Converting arrays to strings and then splitting them.
 var specialCharArr = specialCharStr.split(""); 
 var upperCaseCharArr = uppercaseLetterStr.split("");
 var lowerCaseCharArr = lowercaseLetterStr.split("");
 var numbersArr = numberStr.split("");
-
+// Main Function.
 function generatePassword() {
-
+    // User Prompt.
     var userChoiceLength = prompt("Enter Password Length: ");
-
+    // Alert if user to reach requirements of the choice.
     if (userChoiceLength < 8 || userChoiceLength > 128) { 
         alert("Password must be between 8 and 128 characters")
         return generatePassword()
     }
-
+    // User Confirmation.
     var userChoiceSpChars = confirm("Special Characters?");
 
     var userChoiceUpperCase = confirm("Upper Case?");
@@ -27,12 +27,12 @@ function generatePassword() {
     var userChoiceLowerCase = confirm("Lower Case?");
 
     var userChoiceNumbers = confirm("Numbers?");
-
+    // Alert of user does not choose any sets of characters.
     if (!userChoiceSpChars && !userChoiceUpperCase && !userChoiceLowerCase && !userChoiceNumbers) {
         alert("You must choose at least one set of characters")
         return generatePassword()
     }
-
+    // User Pool, Each are used to pull a character from each set.
     var userCharPool = [];
 
     if (userChoiceSpChars) {
@@ -52,7 +52,7 @@ function generatePassword() {
     }
 
     var myPassword = ""
-
+    // Randomly generating the password one letter or number or special character at a time until the length is completed.
     for (var i = 0; i < userChoiceLength; i++) {
         var index = Math.floor(Math.random() * userCharPool.length);
 
@@ -61,7 +61,7 @@ function generatePassword() {
 
     return myPassword;
 }
-
+// Function to write in the designated Generate password location.
 function writePassword() {
     var password = generatePassword();
     var passwordTextEL = document.getElementById("password");
@@ -69,5 +69,5 @@ function writePassword() {
 
     return;
 }
-
+// Click function to generate password.
 generatePasswordBtn.addEventListener("click", writePassword);
